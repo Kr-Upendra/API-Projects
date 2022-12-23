30cad0f8300fc02fa03316fff4f838938efc8e97
Upendra Kumar <toxic74412@gmail.com>
2022-12-23 21:10:27 +0530

    competes creating file structure

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
 
-app.use();
+app.use(express.json());
+app.use(morgan("short"));
 
 app.get("/", (req, res) => {
   res.status(200).json({
